171b4df2f7eed236a34a74d408141e32d190d01e
18670819116 <365941593@qq.com>
2018-07-03 15:11:17 +0800

    is unuse
    is unuse

--- app/src/main/java/com/stratagile/qlink/constant/MainConstant.java
+++
@@ -1,8 +0,0 @@
-package com.stratagile.qlink.constant;
-
-public class MainConstant {
-    public static String unKownKeyButImportant = "05cd19c64d5f4faabd27c74607fd1f51";
-    public static String MainSign = "ca799da6e13cbf4732b41f050e6574d2eb17f75107da4f53e82c5e25870a91e7";
-    public static String MainAppid = "WINQ";
-    private static String test = "";
-}
